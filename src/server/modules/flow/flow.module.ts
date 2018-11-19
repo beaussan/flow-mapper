51dbc574ce983191eb3046ce48976da89c68538c
Nael <abou.chahine.nael@gmail.com>
2018-11-19 10:19:47 +0100

    :white_check_mark: fix e2e test

--- src/server/modules/flow/flow.module.ts
+++ src/server/modules/flow/flow.module.ts
@@ -4,10 +4,12 @@ import { FlowService } from './flow.service';
 import { FlowApp } from '../flow-app/flow-app.entity';
 import { FlowAppModule } from '../flow-app/flow-app.module';
 import { FlowTechnoModule } from '../flow-techno/flow-techno.module';
+import { FlowAppService } from '../flow-app/flow-app.service';
+import { FlowTechnoService } from '../flow-techno/flow-techno.service';
 
 @Module({
   controllers: [FlowController],
   providers: [FlowService],
-  imports: [FlowAppModule, FlowTechnoModule],
+  imports: [FlowAppModule, FlowTechnoModule, FlowAppModule],
 })
 export class FlowModule {}
