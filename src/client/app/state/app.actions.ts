eab31cfacfca625235cf7e0e5728a3c29d63d9fd
antoine <antoine.rato@gmail.com>
2019-01-11 12:59:36 +0100

    :sparkles: App actions & App state added (Create | Update | Delete)

--- src/client/app/state/app.actions.ts
+++ src/client/app/state/app.actions.ts
@@ -38,3 +38,69 @@ export class FetchOneAppFailure {
 
   constructor(public error: Error) {}
 }
+
+// Create
+export class CreateAppRequest {
+  static readonly type = `${tag} create request`;
+
+  constructor(
+    public name: string,
+    public description: string,
+    public technos: string[],
+  ) {}
+}
+
+export class CreateAppSuccess {
+  static readonly type = `${tag} create success`;
+
+  constructor(public app: App) {}
+}
+
+export class CreateAppError {
+  static readonly type = `${tag} create error`;
+
+  constructor(public error: Error) {}
+}
+
+// Update
+export class UpdateAppRequest {
+  static readonly type = `${tag} update request`;
+
+  constructor(
+    public id: number,
+    public name: string,
+    public description: string,
+    public technos: string[],
+  ) {}
+}
+
+export class UpdateAppSuccess {
+  static readonly type = `${tag} update success`;
+
+  constructor(public app: App) {}
+}
+
+export class UpdateAppError {
+  static readonly type = `${tag} update error`;
+
+  constructor(public error: Error) {}
+}
+
+// Delete
+export class DeleteAppRequest {
+  static readonly type = `${tag} delete request`;
+
+  constructor(public id: number) {}
+}
+
+export class DeleteAppSuccess {
+  static readonly type = `${tag} delete success`;
+
+  constructor(public id: number) {}
+}
+
+export class DeleteAppError {
+  static readonly type = `${tag} delete error`;
+
+  constructor(public error: Error) {}
+}
--- src/client/app/state/app.state.ts
+++ src/client/app/state/app.state.ts
@@ -2,12 +2,21 @@ import { State, Action, StateContext, Selector } from '@ngxs/store';
 import { catchError, flatMap } from 'rxjs/operators';
 import { App } from '../types/app';
 import {
+  CreateAppError,
+  CreateAppRequest,
+  CreateAppSuccess,
+  DeleteAppError,
+  DeleteAppRequest,
+  DeleteAppSuccess,
   FetchAllAppFailure,
   FetchAllAppRequest,
   FetchAllAppSuccess,
   FetchOneAppFailure,
   FetchOneAppRequest,
   FetchOneAppSuccess,
+  UpdateAppError,
+  UpdateAppRequest,
+  UpdateAppSuccess,
 } from './app.actions';
 import { AppService } from '../services/app.service';
 
@@ -70,5 +79,70 @@ export class AppState {
     { currentApp }: FetchOneAppSuccess,
   ) {
     ctx.patchState({ currentApp });
+  }
+
+  // ***** Create ******
+  @Action(CreateAppRequest)
+  createAppRequest(
+    ctx: StateContext<AppStateModel>,
+    { name, description, technos }: CreateAppRequest,
+  ) {
+    return this.appService.addApp(name, description, technos).pipe(
+      flatMap(app => ctx.dispatch(new CreateAppSuccess(app))),
+      catchError(err => ctx.dispatch(new CreateAppError(err))),
+    );
+  }
+
+  @Action(CreateAppSuccess)
+  createAppSuccess(
+    ctx: StateContext<AppStateModel>,
+    { app }: CreateAppSuccess,
+  ) {
+    ctx.patchState({
+      apps: [...ctx.getState().apps, app],
+    });
+  }
+
+  // ***** Update ******
+  @Action(UpdateAppRequest)
+  updateAppRequest(
+    ctx: StateContext<AppStateModel>,
+    { id, name, description, technos }: UpdateAppRequest,
+  ) {
+    return this.appService.updateApp(id, name, description, technos).pipe(
+      flatMap(data => ctx.dispatch(new UpdateAppSuccess(data))),
+      catchError(err => ctx.dispatch(new UpdateAppError(err))),
+    );
+  }
+
+  @Action(UpdateAppSuccess)
+  updateAppSuccess(
+    ctx: StateContext<AppStateModel>,
+    { app }: UpdateAppSuccess,
+  ) {
+    ctx.patchState({
+      apps: ctx.getState().apps.map(f => {
+        if (f.id === app.id) {
+          f.name = app.name;
+        }
+        return f;
+      }),
+    });
+  }
+
+  // ***** Delete ******
+  @Action(DeleteAppRequest)
+  deleteAppRequest(ctx: StateContext<AppStateModel>, { id }: DeleteAppRequest) {
+    return this.appService.deleteApp(id).pipe(
+      flatMap(() => ctx.dispatch(new DeleteAppSuccess(id))),
+      catchError(err => ctx.dispatch(new DeleteAppError(err))),
+    );
+  }
+
+  @Action(DeleteAppSuccess)
+  deleteAppSuccess(ctx: StateContext<AppStateModel>, { id }: DeleteAppSuccess) {
+    ctx.patchState({
+      apps: ctx.getState().apps.filter(f => f.id !== id),
+    });
   }
 }
