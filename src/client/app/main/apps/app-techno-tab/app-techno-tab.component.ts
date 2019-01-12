2bf4df3113ed3043a373bb9544998289ccde24ce
Nael <abou.chahine.nael@gmail.com>
2019-01-12 16:26:48 +0100

    :sparkles: app technos can now be deleted

--- src/client/app/main/apps/app-techno-tab/app-techno-tab.component.ts
+++ src/client/app/main/apps/app-techno-tab/app-techno-tab.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { Select } from '@ngxs/store';
+import { Select, Store } from '@ngxs/store';
 import { AppTechnosState } from '../../../state/app-technos.state';
 import { AppTechno } from '../../../types/app-technos';
 import { Observable } from 'rxjs';
+import { MatDialog } from '@angular/material';
+import { DeleteDialogComponent } from '../../../dialogs/delete-dialog/delete-dialog.component';
+import { DeleteAppTechnoRequest } from '../../../state/app-technos.actions';
 
 @Component({
   selector: 'fl-app-techno-tab',
@@ -15,7 +18,28 @@ export class AppTechnoTabComponent implements OnInit {
 
   displayedColumns: string[] = ['name', 'actions'];
 
-  constructor() {}
+  constructor(public deleteDialog: MatDialog, public store: Store) {}
 
   ngOnInit() {}
+
+  openDialog(appTechno: AppTechno): void {
+    const dialogRef = this.deleteDialog.open(DeleteDialogComponent, {
+      width: '250px',
+      data: {
+        id: appTechno.id,
+        name: appTechno.name,
+        type: 'flow-techno',
+        deleteFunction: this.deleteAppTechno.bind(this),
+      },
+    });
+
+    // dialogRef.afterClosed().subscribe(result => {
+    //   console.log('The dialog was closed');
+    //   this.animal = result;
+    // });
+  }
+
+  deleteAppTechno(id: number) {
+    this.store.dispatch(new DeleteAppTechnoRequest(id));
+  }
 }
