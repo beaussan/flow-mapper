78f377cabc305d77e8eb841dfae8ca882e7723f0
Nael <abou.chahine.nael@gmail.com>
2019-01-12 16:22:36 +0100

    :sparkles: flows can now be deleted

--- src/client/app/main/flows/flows-list-tab/flows-list-tab.component.ts
+++ src/client/app/main/flows/flows-list-tab/flows-list-tab.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { Select } from '@ngxs/store';
+import { Select, Store } from '@ngxs/store';
 import { FlowState } from '../../../state/flow.state';
 import { Observable } from 'rxjs';
 import { Flow } from '../../../types/flow';
+import { DeleteDialogComponent } from '../../../dialogs/delete-dialog/delete-dialog.component';
+import { MatDialog } from '@angular/material';
+import { DeleteFlowRequest } from '../../../state/flows.actions';
 
 @Component({
   selector: 'fl-flows-list-tab',
@@ -21,7 +24,28 @@ export class FlowsListTabComponent implements OnInit {
     'actions',
   ];
 
-  constructor() {}
+  constructor(public deleteDialog: MatDialog, public store: Store) {}
 
   ngOnInit() {}
+
+  openDialog(flow: Flow): void {
+    const dialogRef = this.deleteDialog.open(DeleteDialogComponent, {
+      width: '250px',
+      data: {
+        id: flow.id,
+        name: flow.name,
+        type: 'flow-techno',
+        deleteFunction: this.deleteFlow.bind(this),
+      },
+    });
+
+    // dialogRef.afterClosed().subscribe(result => {
+    //   console.log('The dialog was closed');
+    //   this.animal = result;
+    // });
+  }
+
+  deleteFlow(id: number) {
+    this.store.dispatch(new DeleteFlowRequest(id));
+  }
 }
