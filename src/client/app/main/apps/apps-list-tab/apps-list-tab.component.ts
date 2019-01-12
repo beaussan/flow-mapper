c4ef30a6c96d234c2a1dcf7cc0fa5584b86e56cc
Nael <abou.chahine.nael@gmail.com>
2019-01-12 16:26:28 +0100

    :sparkles: apps can now be deleted

--- src/client/app/main/apps/apps-list-tab/apps-list-tab.component.ts
+++ src/client/app/main/apps/apps-list-tab/apps-list-tab.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { Select } from '@ngxs/store';
-import { AppTechnosState } from '../../../state/app-technos.state';
+import { Select, Store } from '@ngxs/store';
 import { AppState } from '../../../state/app.state';
 import { Observable } from 'rxjs';
 import { App } from '../../../types/app';
+import { MatDialog } from '@angular/material';
+import { DeleteDialogComponent } from '../../../dialogs/delete-dialog/delete-dialog.component';
+import { DeleteAppRequest } from '../../../state/app.actions';
 
 @Component({
   selector: 'fl-apps-list-tab',
@@ -15,7 +17,28 @@ export class AppsListTabComponent implements OnInit {
   apps$: Observable<App[]>;
 
   displayedColumns: string[] = ['name', 'description', 'actions'];
-  constructor() {}
+  constructor(public deleteDialog: MatDialog, public store: Store) {}
 
   ngOnInit() {}
+
+  openDialog(app: App): void {
+    const dialogRef = this.deleteDialog.open(DeleteDialogComponent, {
+      width: '250px',
+      data: {
+        id: app.id,
+        name: app.name,
+        type: 'app',
+        deleteFunction: this.deleteApp.bind(this),
+      },
+    });
+
+    // dialogRef.afterClosed().subscribe(result => {
+    //   console.log('The dialog was closed');
+    //   this.animal = result;
+    // });
+  }
+
+  deleteApp(id: number) {
+    this.store.dispatch(new DeleteAppRequest(id));
+  }
 }
